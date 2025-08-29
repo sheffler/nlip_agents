28e75ba5709e2926895f0f3587331ad18589e389
Thomas Sheffler <tom.sheffler@gmail.com>
2025-08-29 15:30:27 -0700

    Use NLIP Session Server to track conversations

--- nlip_agents/servers/nlipagent_server.py
+++ nlip_agents/servers/nlipagent_server.py
@@ -1,57 +1,60 @@
+#
+# A Server that Launches a NlipAgent instance for each session
+#
+
 import os
 
+from nlip_sdk.nlip import NLIP_Message
 from nlip_sdk.nlip import NLIP_Factory
-from nlip_server import server
-from nlip_sdk import nlip
 
 from nlip_agents.agents.nlip_agent import NlipAgent
+
+import nlip_agents.http_server.nlip_session_server as server
+from nlip_agents.http_server.nlip_session_server import SessionManager
+
 from nlip_agents import logger
+import uvicorn
 
 
-class ChatApplication(server.NLIP_Application):
-    async def startup(self):
-        logger.info("Starting app...")
+#
+# Define a session manager that launches a new BasicAgent for each session
+#
+
+class NlipManager(SessionManager):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
         self.myAgent = NlipAgent(
             "Margaret"
         )
 
-    async def shutdown(self):
-        return None
+    async def process_nlip(self, msg: NLIP_Message) -> NLIP_Message:
 
-    async def create_session(self) -> server.NLIP_Session:
-        return ChatSession(self.myAgent)
-
-
-class ChatSession(server.NLIP_Session):
-
-    def __init__(self, agent: NlipAgent):
-        self.agent = agent
-
-    async def start(self):
-        logger.info("Starting the chat session")
-
-    async def execute(
-        self, msg: nlip.NLIP_Message
-    ) -> nlip.NLIP_Message:
-        logger = self.get_logger()
+        # concatenate all of the "text" parts
         text = msg.extract_text()
 
         try:
-            results = await self.agent.process_query(text)
+            results = await self.myAgent.process_query(text)
+            logger.info(f"Results : {results}")
             msg = NLIP_Factory.create_text(results[0])
             for res in results[1:]:
                msg.add_text(res)
-            print(f"MSG:{msg}")
             return msg
-
         except Exception as e:
             logger.error(f"Exception {e}")
             return None
+        
+        
+#
+# Now configure the server
+#
 
-    async def stop(self):
-        logger.info(f"Stopping chat session")
-        await self.agent.cleanup()
-        self.server = None
+server.SESSION_MANAGER_CLASS = NlipManager
+server.SESSION_COOKIE_NAME = "NlipCoordinatorCookie"
 
+app = server.app
 
-app = server.setup_server(ChatApplication())
+if __name__ == "__main__":
+    # uvicorn.run(app, host="0.0.0.0", port=8024, log_level="info")
+    uvicorn.run("nlip_agents.servers.nlip_server:app", host="0.0.0.0", port=8024, log_level="info", reload=True)
